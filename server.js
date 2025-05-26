875e47bb7f7680a99ba21d0ad67550e520b8c6cb
Phan Tan truc <phantruc438@gmail.com>
2025-05-26 23:33:25 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const { trainAndPredictFromInput } = require('./lib/regresstion.js');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.post('/predict', async (req, res) => {
+  try {
+    const {new_ad} = req.body;
+    console.log(new_ad);
+    
+    const result = await trainAndPredictFromInput(new_ad);
+    res.json(result);
+  } catch (err) {
+    console.error('❌ Lỗi dự đoán:', err);
+    res.status(500).json({ error: 'Lỗi xử lý' });
+  }
+});
+
+app.get('/', (req, res) => {
+  res.send('👍 API Predict hoạt động');
+});
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server đang chạy ở cổng ${PORT}`);
+});
---
+++ test.js
@@ -0,0 +1,3 @@
+// test.js
+const fs = require('fs');
+console.log('✅ Require hoạt động');
